da376cd885d51fea9dc1d0cc0bfa0de0b6bf47ae
mohammed S <moha684@gmail.com>
2019-08-21 22:45:29 +0300

    days 2

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-document.write("مرحبا بك عالم الجافا سكربت");
-document.write( 5 + 6);
+
+
+//this is my first code 
+
+/*this is my first code 
+and I need to write more comments
+document.write("النص هنا لا يتم طباعة لانه داخل التعليقات");
+*/
+document.write("مرحباً بكم عالم الجافا سيكربت");
+
+console.log('hello World');
+
